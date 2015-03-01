e9640c431880b9f002853b6580f411c7d5b89f2c
Saravanan <sjs7007@gmail.com>
2015-03-02 01:12:42 +0530

    created csv out of some batch runs. use for analysis

--- PaperImplementation/Eclipse/ML/src/ml/batchTest2.java
+++ PaperImplementation/Eclipse/ML/src/ml/batchTest2.java
@@ -1,6 +1,5 @@
 package ml;
 
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -40,9 +39,12 @@ public class batchTest2
 		Date start,end;
 		String ip,executionTime;
 		long executionTimeMS;
+		//StringBuffer logData = new StringBuffer();
+
 		
 		System.out.println("Test Number : "+count);
 		System.out.println("N="+N+",C_h="+C_h+",C_s="+C_s+",C_v="+C_v);
+		//logData.append("Test Number : "+count+"\n"+"N="+N+",C_h="+C_h+",C_s="+C_s+",C_v="+C_v+"\n");
 		
 		//extract train features
 		ip = new String("/home/shinchan/graphAccuracy/200TrainResized");
@@ -52,6 +54,7 @@ public class batchTest2
 		executionTimeMS = end.getTime()-start.getTime(); //in milliseconds
 		executionTime = Float.toString((float)executionTimeMS/(60*1000))+" minutes";
 		System.out.print("Time taken for feature extraction from training images: "+executionTime+"\n");
+		//logData.append("Time taken for feature extraction from training images: "+executionTime+"\n");
 		
 		//extract test features
 		ip = new String("/home/shinchan/graphAccuracy/ga2");
@@ -61,6 +64,7 @@ public class batchTest2
 		executionTimeMS = end.getTime()-start.getTime(); //in milliseconds
 		executionTime = Float.toString((float)executionTimeMS/(60*1000))+" minutes";
 		System.out.print("Time taken for feature extraction from test images: "+executionTime+"\n");
+		//logData.append("Time taken for feature extraction from test images: "+executionTime+"\n");
 		
 		
 
@@ -68,7 +72,10 @@ public class batchTest2
 	    File trainFeatures = new File("results/batchTest.train");
 	    File testFeatures = new File("results/batchTest.test");
 	    File modelFile = new File("results/batchTest.model");
-		String resultsName = "results/batchTest.result";				
+	    File logFile = new File("results/batchTest.log");
+		String resultsName = "results/batchTest.result";		
+    	FileWriter op = new FileWriter(logFile.toString());
+
 
 		
 		//generate model
@@ -79,6 +86,7 @@ public class batchTest2
 		executionTimeMS = end.getTime()-start.getTime(); //in milliseconds
 		executionTime = Float.toString((float)executionTimeMS/(60*1000))+" minutes";
 		System.out.print("Time taken to generate model: "+executionTime+"\n");
+		//logData.append("Time taken to generate model: "+executionTime+"\n");
 
 		//prediction
 		start =new Date();
@@ -88,6 +96,7 @@ public class batchTest2
 		executionTimeMS = end.getTime()-start.getTime(); //in milliseconds
 		executionTime = Float.toString((float)executionTimeMS/(60*1000))+" minutes";
 		System.out.print("Time taken for prediction : "+executionTime+"\n----x----\n");
+		//logData.append("Time taken for prediction : "+executionTime+"\n----x----\n");
 		
 		
 	}
