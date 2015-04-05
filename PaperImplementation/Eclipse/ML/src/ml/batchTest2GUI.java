04868e0ebc54af07416bcd51ba71942478586d1d
Saravanan <sjs7007@gmail.com>
2015-04-05 20:55:02 +0530

    batch test GUI done.

--- PaperImplementation/Eclipse/ML/src/ml/batchTest2GUI.java
+++ PaperImplementation/Eclipse/ML/src/ml/batchTest2GUI.java
@@ -3,12 +3,17 @@ package ml;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 
@@ -22,11 +27,15 @@ public class batchTest2GUI extends JFrame {
 	private JTextField C_sStart;
 	private JTextField C_sEnd;
 	private JTextField C_vStart;
-	private JTextField textField_5;
+	private JTextField C_vEnd;
 	private JLabel lblImageDimension;
-	private JTextField textField_6;
+	private JTextField imageDimension;
 	private JButton btnRun;
-	private JButton btnForceStop;
+	
+	JFileChooser fc;
+	File trainData = new File("/home/shinchan/FinalProject/PaperImplementation/Eclipse/ML/input/trainingImageResized"),testData =null,inputFileList=null,extractedFeatures=null,trainFeatures=null, testFeatures=null, modelFile=null;
+
+
 
 	/**
 	 * Launch the application.
@@ -48,6 +57,13 @@ public class batchTest2GUI extends JFrame {
 	 * Create the frame.
 	 */
 	public batchTest2GUI() {
+		
+		// Create a file chooser
+		//user.dir is to get defualt directory
+		fc =  new JFileChooser(new File(System.getProperty("user.dir")));
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); 
+
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 391, 412);
 		contentPane = new JPanel();
@@ -55,17 +71,39 @@ public class batchTest2GUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnSele = new JButton("Select Training Folder");
-		btnSele.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		JButton btnSelectTrainingFolder = new JButton("Select Training Folder");
+		btnSelectTrainingFolder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {	
+				
+				int returnVal = fc.showOpenDialog(batchTest2GUI.this);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					trainData = fc.getSelectedFile();
+					// This is where a real application would open the file.
+					System.out.print("Folder selected for training : " + trainData.toString() + ".\n");				
+				} else {
+					System.out.print("Open command cancelled by user.\n");
+				}
+				//log.setCaretPosition(log.getDocument().getLength());
 			}
 		});
-		btnSele.setBounds(84, 12, 226, 25);
-		contentPane.add(btnSele);
+		btnSelectTrainingFolder.setBounds(84, 12, 226, 25);
+		contentPane.add(btnSelectTrainingFolder);
 		
 		JButton btnSelectTestFolder = new JButton("Select Test Folder");
 		btnSelectTestFolder.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				int returnVal = fc.showOpenDialog(batchTest2GUI.this);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					testData = fc.getSelectedFile();
+					//This is where a real application would open the file.
+					System.out.print("Folder selected for testing : " + testData.getName() + ".\n");
+
+				} else {
+					System.out.print("Open command cancelled by user.\n" );
+				}
 			}
 		});
 		btnSelectTestFolder.setBounds(84, 54, 226, 25);
@@ -78,7 +116,7 @@ public class batchTest2GUI extends JFrame {
 		nStart.setColumns(10);
 		
 		nEnd = new JTextField();
-		nEnd.setText("10");
+		nEnd.setText("5");
 		nEnd.setColumns(10);
 		nEnd.setBounds(265, 132, 45, 19);
 		contentPane.add(nEnd);
@@ -106,7 +144,7 @@ public class batchTest2GUI extends JFrame {
 		contentPane.add(C_hStart);
 		
 		C_hEnd = new JTextField();
-		C_hEnd.setText("10");
+		C_hEnd.setText("5");
 		C_hEnd.setColumns(10);
 		C_hEnd.setBounds(265, 169, 45, 19);
 		contentPane.add(C_hEnd);
@@ -122,7 +160,7 @@ public class batchTest2GUI extends JFrame {
 		contentPane.add(C_sStart);
 		
 		C_sEnd = new JTextField();
-		C_sEnd.setText("10");
+		C_sEnd.setText("5");
 		C_sEnd.setColumns(10);
 		C_sEnd.setBounds(265, 200, 45, 19);
 		contentPane.add(C_sEnd);
@@ -137,28 +175,55 @@ public class batchTest2GUI extends JFrame {
 		C_vStart.setBounds(118, 231, 45, 19);
 		contentPane.add(C_vStart);
 		
-		textField_5 = new JTextField();
-		textField_5.setText("10");
-		textField_5.setColumns(10);
-		textField_5.setBounds(265, 231, 45, 19);
-		contentPane.add(textField_5);
+		C_vEnd = new JTextField();
+		C_vEnd.setText("5");
+		C_vEnd.setColumns(10);
+		C_vEnd.setBounds(265, 231, 45, 19);
+		contentPane.add(C_vEnd);
 		
 		lblImageDimension = new JLabel("Image Dimension");
 		lblImageDimension.setBounds(86, 286, 128, 15);
 		contentPane.add(lblImageDimension);
 		
-		textField_6 = new JTextField();
-		textField_6.setText("250");
-		textField_6.setBounds(224, 286, 53, 17);
-		contentPane.add(textField_6);
-		textField_6.setColumns(10);
+		imageDimension = new JTextField();
+		imageDimension.setText("250");
+		imageDimension.setBounds(224, 286, 53, 17);
+		contentPane.add(imageDimension);
+		imageDimension.setColumns(10);
 		
 		btnRun = new JButton("Run");
-		btnRun.setBounds(53, 329, 110, 25);
+		btnRun.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)  {
+				int count=1;
+				
+				for(int n=Integer.parseInt(nStart.getText());n<=Integer.parseInt(nEnd.getText());n=n+1)
+				{
+					for(int h=Integer.parseInt(C_hStart.getText());h<=Integer.parseInt(C_hEnd.getText());h=h+1)
+					{
+						for(int s=Integer.parseInt(C_sStart.getText());s<=Integer.parseInt(C_sEnd.getText());s=s+1)
+						{
+							for(int v=Integer.parseInt(C_vStart.getText());v<=Integer.parseInt(C_vEnd.getText());v=v+1)
+							//int h=10,s=10,v=10;
+							{
+								try {
+									//System.out.print(imageDimension.getText());
+									batchTest2_gui_mod.predictM(count,n,h,s,v,Integer.parseInt(imageDimension.getText()), trainData.toString(), testData.toString());
+								} catch (NumberFormatException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								} catch (IOException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+								count++;
+							}
+						}                                                                                                     
+					}
+					
+				}
+			}
+		});
+		btnRun.setBounds(129, 332, 110, 25);
 		contentPane.add(btnRun);
-		
-		btnForceStop = new JButton("Force Stop");
-		btnForceStop.setBounds(213, 329, 110, 25);
-		contentPane.add(btnForceStop);
 	}
 }
---
+++ PaperImplementation/Eclipse/ML/src/ml/batchTest2_gui_mod.java
@@ -0,0 +1,108 @@
+package ml;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+public class batchTest2_gui_mod
+{
+	public static void main(String args[]) throws IOException
+	{
+		int N=5,C_h=10,C_s=6,C_v=6;
+		/*for 250 reized data*/ // int cellDimension=250;
+		/*for 32*32 data*/ int cellDimension=32;
+		int count=1;
+		
+		for(int n=5;n<=10;n=n+1)
+		{
+			for(int h=5;h<=10;h=h+1)
+			{
+				for(int s=5;s<=10;s=s+1)
+				{
+					for(int v=5;v<=10;v=v+1)
+					//int h=10,s=10,v=10;
+					{
+						predictM(count,n,h,s,v,cellDimension, null, null);
+						count++;
+					}
+				}                                                                                                     
+			}
+			
+		}
+		
+		//predictM(count,10,8,6,6);
+		
+	
+                                                               
+
+	}
+	
+	public static void predictM(int count,int N,int C_h,int C_s,int C_v,int cellDimension, String trainFolder, String testFolder) throws IOException
+	{
+		Date start,end;
+		String ip,executionTime;
+		long executionTimeMS;
+		//StringBuffer logData = new StringBuffer();
+
+		
+		System.out.println("Test Number : "+count);
+		System.out.println("N="+N+",C_h="+C_h+",C_s="+C_s+",C_v="+C_v);
+		//logData.append("Test Number : "+count+"\n"+"N="+N+",C_h="+C_h+",C_s="+C_s+",C_v="+C_v+"\n");
+		
+		//extract train features
+		//ip = new String("/home/shinchan/graphAccuracy/200TrainResized");
+		
+		start =new Date();	    
+	    ML.batchColorFeatureBuilder(trainFolder, N, C_h, C_s, C_v,new File("batchTest.train"),new File("batchTest.list"), cellDimension); 
+	    end = new Date();		
+		executionTimeMS = end.getTime()-start.getTime(); //in milliseconds
+		executionTime = Float.toString((float)executionTimeMS/(60*1000))+" minutes";
+		System.out.print("Time taken for feature extraction from training images: "+executionTime+"\n");
+		//logData.append("Time taken for feature extraction from training images: "+executionTime+"\n");
+		
+		//extract test features
+		//ip = new String("/home/shinchan/graphAccuracy/ga3");
+		start =new Date();
+	    ML.batchColorFeatureBuilder(testFolder, N, C_h, C_s, C_v,new File("batchTest.test"),new File("batchTest.list"), cellDimension); 
+	    end = new Date();
+		executionTimeMS = end.getTime()-start.getTime(); //in milliseconds
+		executionTime = Float.toString((float)executionTimeMS/(60*1000))+" minutes";
+		System.out.print("Time taken for feature extraction from test images: "+executionTime+"\n");
+		//logData.append("Time taken for feature extraction from test images: "+executionTime+"\n");
+		
+		
+
+	    //file declarations
+	    File trainFeatures = new File("results/batchTest.train");
+	    File testFeatures = new File("results/batchTest.test");
+	    File modelFile = new File("results/batchTest.model");
+	    File logFile = new File("results/batchTest.log");
+		String resultsName = "results/batchTest.result";		
+    	FileWriter op = new FileWriter(logFile.toString());
+
+
+		
+		//generate model
+		start =new Date();
+		String temp[]={trainFeatures.toString(),modelFile.toString()};
+		svm_train.main(temp);
+	    end = new Date();
+		executionTimeMS = end.getTime()-start.getTime(); //in milliseconds
+		executionTime = Float.toString((float)executionTimeMS/(60*1000))+" minutes";
+		System.out.print("Time taken to generate model: "+executionTime+"\n");
+		//logData.append("Time taken to generate model: "+executionTime+"\n");
+
+		//prediction
+		start =new Date();
+		String argv[]={testFeatures.toString(),modelFile.toString(),resultsName};
+		svm_predict.main(argv);
+		end = new Date();
+		executionTimeMS = end.getTime()-start.getTime(); //in milliseconds
+		executionTime = Float.toString((float)executionTimeMS/(60*1000))+" minutes";
+		System.out.print("Time taken for prediction : "+executionTime+"\n----x----\n");
+		//logData.append("Time taken for prediction : "+executionTime+"\n----x----\n");
+		
+		
+	}
+}
